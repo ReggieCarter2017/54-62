38343ae3bbc03e94c6f3af041ba57f09ad1dd5c5
coolpagani <coolpagani@gmail.com>
2022-09-09 14:01:22 +0300

    work_54

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿int[,] CreatinveMassive(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int s = 0; s < array.GetLength(1) - 1; s++)
+            {
+                if (array[i, s] < array[i, s + 1])
+                {
+                int temp = array[i, s + 1];
+                array[i, s + 1] = array[i, s];
+                array[i, s] = temp;
+                }
+            }
+        }
+    }
+    return array;   
+}
+int a = 5;
+int b = 6;
+CreatinveMassive(a, b);
+
+void PrintArray(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            Console.Write($"{a[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(CreatinveMassive(a, b));
+
