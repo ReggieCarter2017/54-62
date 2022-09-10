ffb8ad600cadb2a31369fa73e5fa0ce170acf296
coolpagani <coolpagani@gmail.com>
2022-09-10 20:20:58 +0300

    work_62

--- Program.cs
+++ Program.cs
@@ -1,33 +1,48 @@
-﻿
-int a = 2;
-int b = 2;
-int c = 2;
-int[,,] CreatingMassive(int m, int n, int s)
-{
-    int[,,] array = new int[m, n, s];
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            for (int k = 0; k < array.GetLength(2); k++)
-            {
-                array[i, j, k] = new Random().Next(10, 100);
-            }
-        }
-    }
+﻿int s = 4;
 
-    return array;   
-}
 
-int[,,] array3diment = CreatingMassive(a, b, c);
-for (int i = 0; i < array3diment.GetLength(0); i++)
-{
-    for (int j = 0; j < array3diment.GetLength(1); j++)
-    {
-        for (int k = 0; k < array3diment.GetLength(2); k++)
-        {
-            Console.Write($"{array3diment[i, j, k]} ({i}, {j}, {k}) ");
-        }
-            Console.WriteLine();
-    }
-}
+    int[,] array = new int[s, s];
+    int i = 0;
+    int x = 0;
+    int y = -1;
+    int uD = 0;
+    int rL = 1;
+   
+            while (i < s*s)
+                {
+                    if (0 <= x + uD && x + uD < s && 0 < y + rL && y + rL < s && array[x + uD, y + rL] == 0)
+                    {
+                        x += uD;
+                        y += rL;
+                        array[x, y] = i;
+                        i++;
+                        Console.Write(i + " ");
+
+                    }
+                    else
+                    {
+                        if(rL == 1)
+                        {
+                            rL = 0;
+                            uD = 1;
+                        }
+                        else if (uD == 1)
+                        {
+                            uD = 0;
+                            rL = -1;
+                        }
+                        else if (rL == -1)
+                        {
+                            rL = 0;
+                            uD = -1;
+                        }
+                        else if (uD == -1)
+                        {
+                            uD = 0;
+                            rL = 1;
+                        }
+                        i++;
+                    }   
+                }
+
+
